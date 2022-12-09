0096bd58a4c95d7cf758822b1bb9c9a9f3eaddb2
dkamundi <davekamundi@gmail.com>
2022-12-09 08:07:25 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -12,7 +12,7 @@ int main(void)
 	c = 0;
 	while
 		(c < 10) {
-			putchar(c +'0');
+			putchar(c + '0');
 			c++;
 		}
 	putchar('\n');
