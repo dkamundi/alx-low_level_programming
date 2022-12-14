0a47f6e0e8d43a03565dcdf71aad802549f990a6
dkamundi <davekamundi@gmail.com>
2022-12-14 15:06:02 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,13 +14,13 @@ int print_sign(int n)
 	return (1);
 	}
 	else if (n < 0)
-        {       
-        _putchar(45);
-        return (-1);
+	{
+	_putchar(45);
+	return (-1);
 	}
 	else
 	{
 	_putchar(48);
-        return (0);
+	return (0);
 	}
 }
