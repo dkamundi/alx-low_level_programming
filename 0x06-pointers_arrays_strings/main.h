4eb3e063070a892fac1c33df0535af1b16c415cc
dkamundi <davekamundi@gmail.com>
2022-12-22 16:20:33 -0800

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
          * int prototypes()
          * char prototypes()
          */
-        void reserve_array(int *a, int n);
+        void reverse_array(int *a, int n);
         void print_number(int n);
         void print_buffer(char *b, int size);
         int _strcmp(char *s1, char *s2);
